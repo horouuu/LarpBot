ed45e568cc0537b3dad2845a283bdcbaf6637772
horouuu <fernandafadja@gmail.com>
2025-10-01 06:51:46 +0800

    feat(commands): include config context

--- src/types/commands.d.ts
+++ src/types/commands.d.ts
@@ -1,8 +1,9 @@
+import { Config } from "@config";
 import { CacheType, Interaction, ApplicationCommandOption } from "discord.js";
 
 export interface Command {
   name: string;
   description: string;
   options?: ApplicationCommandOption[];
-  execute: (interaction: Interaction<CacheType>) => any;
+  execute: (interaction: Interaction<CacheType>, config?: Config) => any;
 }
