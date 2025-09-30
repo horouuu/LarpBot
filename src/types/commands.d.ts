7edabe7b072a6bb1dfa60d5c24f3a278942c51df
horouuu <fernandafadja@gmail.com>
2025-10-01 03:04:55 +0800

    feat: update Commands type to include options

--- src/types/commands.d.ts
+++ src/types/commands.d.ts
@@ -1,7 +1,8 @@
-import { CacheType, Interaction } from "discord.js";
+import { CacheType, Interaction, ApplicationCommandOption } from "discord.js";
 
 export interface Command {
   name: string;
   description: string;
+  options?: ApplicationCommandOption[];
   execute: (interaction: Interaction<CacheType>) => any;
 }
