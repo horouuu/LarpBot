b953bb3491b7b30fbeb770a610492e2c79b6f6b3
horouuu <fernandafadja@gmail.com>
2025-10-02 14:42:50 +0800

    feat: basic redis wrapper class

---
+++ src/redis/RedisStorage.ts
@@ -0,0 +1,48 @@
+import { createClient, RedisClientType, RedisModules } from "redis";
+import { Config } from "@config";
+
+type PersistedConfigs = {
+  actionThreshold?: Config["actionThreshold"];
+};
+
+export class RedisStorage {
+  private client: ReturnType<typeof createClient>;
+
+  private constructor(client: typeof this.client) {
+    this.client = client;
+  }
+
+  public static async create(config: Config) {
+    const client = createClient({
+      password: config.redisPassword,
+    });
+
+    client.on("err", (e) => console.error(e));
+    await client.connect();
+
+    return new RedisStorage(client);
+  }
+
+  private async set(key: string, value: string): Promise<void> {
+    try {
+      await this.client.set(key, value);
+    } catch (e) {
+      console.error(e);
+      throw new Error("Failed to write to database!");
+    }
+  }
+
+  private async get(key: string): Promise<string> {
+    try {
+      const res = (await this.client.get(key)) as string;
+      return res;
+    } catch (e) {
+      console.error(e);
+      throw new Error("Failed to fetch from database!");
+    }
+  }
+
+  public async destroy(): Promise<void> {
+    await this.client.close();
+  }
+}
