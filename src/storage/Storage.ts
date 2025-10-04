f7bec47e7068ac76a3c100eb398e915f8bf49664
horouuu <fernandafadja@gmail.com>
2025-10-04 13:34:45 +0800

    feat(storage): changed gatekeeper funcs to use string instead of set

--- src/storage/Storage.ts
+++ src/storage/Storage.ts
@@ -1,9 +1,10 @@
-import { AtLeastOne, OrNullEntries } from "@/types/util";
+import { AtLeastOne, OrNullEntries } from "@types-local/util";
 import { ConfigType } from "@config";
 
 const persistedConfigs = ["actionThreshold"] as const;
 type PersistedKey = (typeof persistedConfigs)[number];
 type PersistedConfigs = Pick<ConfigType, PersistedKey>;
+type ChRegReturn = { success: true } | { success: false; watching: string };
 export abstract class Storage {
   abstract registerConfigs(
     guildId: string,
@@ -13,6 +14,19 @@ export abstract class Storage {
   abstract retrieveConfigs(
     guildId: string
   ): Promise<OrNullEntries<PersistedConfigs>>;
+
+  abstract chRegGatekeeper(
+    guildId: string,
+    channelId: string,
+    force?: boolean
+  ): Promise<ChRegReturn>;
+
+  abstract chGetGatekeeper(guildId: string): Promise<string | null>;
+
+  abstract chDelGatekeeper(
+    guildId: string,
+    channelId: string
+  ): Promise<{ success: boolean }>;
 }
 
 export type { PersistedKey, PersistedConfigs };
