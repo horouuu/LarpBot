87bfb9f650fa67e0456a9240f0dca56db3814066
horouuu <fernandafadja@gmail.com>
2025-10-04 15:56:21 +0800

    feat(handlers/gatekeepers): add an event loader for bot restarts

---
+++ src/handlers/gatekeeper/gatekeeper-event-loader.ts
@@ -0,0 +1,24 @@
+import { HandlerContext } from "@types-local/global";
+import { createGatekeeperCollectors } from "./gatekeeper-reactions";
+import { TextChannel } from "discord.js";
+
+export async function gatekeeperEventLoader(ctx: HandlerContext) {
+  const { client, storage } = ctx;
+  const gatekeptEntries = await storage.getAllGatekept();
+
+  for (const gatekept of gatekeptEntries) {
+    const channel = (await client.channels.fetch(
+      gatekept.channelId
+    )) as TextChannel;
+
+    if (!channel) {
+      console.warn(`Failed to fetch channel: ${channel}.`);
+      continue;
+    }
+
+    await createGatekeeperCollectors({ ...ctx, channel });
+    console.log(
+      `[Gatekeeper Loader]: Successfully attached collectors for guild ${gatekept.guildId}`
+    );
+  }
+}
