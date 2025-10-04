ccf1d2a07d6fac6d6850b4c9dab70732ed01a06c
horouuu <fernandafadja@gmail.com>
2025-10-04 15:58:16 +0800

    feat: run event loader once ready

--- src/bot.ts
+++ src/bot.ts
@@ -4,10 +4,10 @@ import { Config } from "@config";
 import { commandHandler, initCommands } from "@handlers/command-handler.js";
 import { RedisStorage } from "@redis-storage";
 import { HandlerContext } from "@types-local/global";
+import { gatekeeperEventLoader } from "@handlers/gatekeeper/gatekeeper-event-loader";
 
 const config = new Config();
 const storage = await RedisStorage.create(config);
-const handlerCtx: HandlerContext = { config, storage };
 
 const client = new Client({
   intents: [
@@ -20,10 +20,12 @@ const client = new Client({
   ],
   partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
+const handlerCtx: HandlerContext = { client, config, storage };
 
 // ready listener
-client.on(Events.ClientReady, async (readyClient) => {
+client.once(Events.ClientReady, async (readyClient) => {
   console.log(`Successfully logged in as ${readyClient.user.tag}!`);
+  await gatekeeperEventLoader(handlerCtx);
 });
 
 const commands = await initCommands(config);
