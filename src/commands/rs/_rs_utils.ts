404d4c8f557d7ab54ba041dee1591049dcb52890
horouuu <fernandafadja@gmail.com>
2025-10-12 08:53:46 +0800

    feat(rs-save): rs utils for updating clue data

--- src/commands/rs/_rs_utils.ts
+++ src/commands/rs/_rs_utils.ts
@@ -1,4 +1,35 @@
+import { RedisStorage } from "@redis-storage";
 import { Item, Util } from "oldschooljs";
+
+type DBClueData = {
+  medium: number;
+  hard: number;
+  elite: number;
+  master: number;
+  clue_coins: number;
+};
+
+function getEmptyClueData(): DBClueData {
+  return {
+    medium: 0,
+    hard: 0,
+    elite: 0,
+    master: 0,
+    clue_coins: 0,
+  } as DBClueData;
+}
+
+function getClueKey(userId: string) {
+  return `users:${userId}:rs:clues`;
+}
+
+function getCoinsKey(userId: string) {
+  return `users:${userId}:rs:coins`;
+}
+
+function getKcKey(userId: string, monsterId: number) {
+  return `user:${userId}:rs:kills:${monsterId}`;
+}
 
 export function parseLoot(rewards: [Item, number][]) {
   let total = 0;
@@ -16,5 +47,28 @@ export function parseLoot(rewards: [Item, number][]) {
     .join("\n");
 
   const totalStr = Util.toKMB(total);
-  return { got, total: totalStr };
+  return { got, total: totalStr, totalRaw: total };
+}
+
+export async function updateClueData(
+  storage: RedisStorage,
+  userId: string,
+  data: DBClueData
+) {
+  const clueKey = getClueKey(userId);
+  await storage.hIncrByFields(clueKey, data);
+}
+
+export async function getClueData(storage: RedisStorage, userId: string) {
+  const clueKey = getClueKey(userId);
+  return await storage.hGetAll(clueKey);
+}
+
+export async function updateCoins(
+  storage: RedisStorage,
+  userId: string,
+  change: number
+) {
+  const coinsKey = getCoinsKey(userId);
+  await storage.set(coinsKey, change.toString());
 }
